b40f5421a00be0dba0655d066e4f5ba772fc4bc5
Nathan Shapter <shapternathan@protonmail.com>
2024-11-23 20:40:33 -0500

    somehow broke player animator, objects randomly spawned for decoration

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+  [SerializeField]  GameObject[] boxes;
+    [SerializeField] GameObject[] seaWeed;
+    [SerializeField] GameObject[] corals;
+    [SerializeField] GameObject[] decor;
+    [SerializeField] GameObject[] shadows;
+
+}
