18305e275849e8f6c958e6ac7d6a727ad0a6f0ab
Nathan Shapter <shapternathan@protonmail.com>
2024-11-27 06:32:35 -0500

    broken push, yay

---
+++ Assets/Scripts/Level Settings/LevelSettings.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLevelSettings", menuName = "Level Settings", order = 1)]
+public class LevelSettings : ScriptableObject
+{
+    [Header("Spawn Settings")]
+    [SerializeField] private int swordFishToSpawn;
+    [SerializeField] private int turtlesToSpawn;
+    [SerializeField] private int jellyFishToSpawn;
+    [SerializeField] private int octopusToSpawn;
+    [SerializeField] private int eelToSpawn;
+    [SerializeField] private int sharkToSpawn;
+
+    // Public properties to access the fields if needed
+    public int SwordFishToSpawn => swordFishToSpawn;
+    public int TurtlesToSpawn => turtlesToSpawn;
+    public int JellyFishToSpawn => jellyFishToSpawn;
+    public int OctopusToSpawn => octopusToSpawn;
+    public int EelToSpawn => eelToSpawn;
+    public int SharkToSpawn => sharkToSpawn;
+}
